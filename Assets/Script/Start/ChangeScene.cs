f16c8a90b177c48fa6001d290e8a4018b5a09e8f
Hoang Trung Nguyen <hoangtrungnguyen149@gmail.com>
2026-09-12 21:56:22 +0700

    done all animation + scene start

---
+++ Assets/Script/Start/ChangeScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // Bắt buộc phải có thư viện này
+
+public class ChangeScene : MonoBehaviour
+{
+    // Hàm này phải là public để nút bấm (Button) có thể gọi được
+    public void LoadGameScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
